7c39da416523a868ad0f796fcc365e835fa698bd
宅の士 <HouseMen@foxmail.com>
2026-02-06 17:18:13 +0800

    修复arm-linux-uclibcgnueabihf-clang编译链接故障

--- cmake/armv7a-linux-uclibcgnueabihf-clang.cmake
+++ cmake/armv7a-linux-uclibcgnueabihf-clang.cmake
@@ -5,6 +5,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7-a)
 
 set(CLANG_TARGET armv7a-none-linux-gnueabihf)
+add_link_options(-Wl,-dynamic-linker=/lib/ld-uClibc.so.0)
 
 set(CMAKE_THREAD_LIBS_INIT -lpthread)
 set(CMAKE_HAVE_THREADS_LIBRARY ON)
