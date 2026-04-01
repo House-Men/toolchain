7324e98f5715ff2f1b5d0e7e999a28d447535ec5
宅の士 <HouseMen@foxmail.com>
2026-04-01 13:13:12 +0800

    调整工具链脚本固定依赖路径为环境变量

--- arm-linux-gnueabihf-clang.cmd
+++ arm-linux-gnueabihf-clang.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-clang -fuse-ld=lld --target=arm-linux-gnueabihf --sysroot=E:\toolchain\gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf\arm-linux-gnueabihf\libc --gcc-toolchain=E:\toolchain\gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf %*
+clang -fuse-ld=lld --target=arm-linux-gnueabihf --sysroot=%TOOLCHAIN_HOME%\gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf\arm-linux-gnueabihf\libc --gcc-toolchain=%TOOLCHAIN_HOME%\gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf %*
 if ERRORLEVEL 1 exit /b 1
--- x86_64-linux-gnu-clang++.cmd
+++ x86_64-linux-gnu-clang++.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-clang++ -fuse-ld=lld --target=x86_64-pc-linux-gnu --sysroot=E:\toolchain\x86_64-debian10-linux-gnu --gcc-toolchain=E:\toolchain\x86_64-debian10-linux-gnu\usr %*
+clang++ -fuse-ld=lld --target=x86_64-pc-linux-gnu --sysroot=%TOOLCHAIN_HOME%\x86_64-debian10-linux-gnu --gcc-toolchain=%TOOLCHAIN_HOME%\x86_64-debian10-linux-gnu\usr %*
 if ERRORLEVEL 1 exit /b 1
