1345c5df978b16bff3e81d8e1542ae9ae710ead5
宅の士 <HouseMen@foxmail.com>
2026-04-03 02:08:36 +0800

    支持WSL环境MSVC编译

--- i686-msvc-clang.cmd
+++ i686-msvc-clang.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-clang --target=i686-w64-windows-msvc -fuse-ld=lld %*
+clang --target=i686-w64-windows-msvc -fuse-ld=lld -lmsvcrt %*
 if ERRORLEVEL 1 exit /b 1
--- x86_64-msvc-clang.cmd
+++ x86_64-msvc-clang.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-clang --target=x86_64-pc-windows-msvc -fuse-ld=lld %*
+clang --target=x86_64-pc-windows-msvc -fuse-ld=lld -lmsvcrt %*
 if ERRORLEVEL 1 exit /b 1
